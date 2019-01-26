dc507086406b3042bc3370dc11e6867dccbcfa29
Charles <conf272@soton.ac.uk>
2019-01-26 12:14:17 +0000

    Add traffic AI

---
+++ GhostInTheSushi/Assets/Scripts/OtherVehicleDetector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherVehicleDetector : MonoBehaviour {
+
+    public TrafficVehicle Vehicle;
+
+    private TrafficVehicle vehicleInFront;
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        var trafficAI = other.GetComponent<TrafficVehicle>();
+
+        if (trafficAI)
+        {
+            Vehicle.Stop(this.gameObject);
+            vehicleInFront = trafficAI;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var trafficAI = other.GetComponent<TrafficVehicle>();
+
+        if (trafficAI && trafficAI == vehicleInFront)
+        {
+            vehicleInFront = null;
+            Vehicle.Continue(this.gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // If the vehicle in front is deleted then continue
+        if (vehicleInFront == null)
+            Vehicle.Continue(this.gameObject);
+    }
+}
