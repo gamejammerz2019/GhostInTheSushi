4664a177493fb5f733da0ab2e05bb2fbb15f2389
Charles <conf272@soton.ac.uk>
2019-01-26 10:16:55 +0000

    Add pedestrian script

---
+++ GhostInTheSushi/Assets/Scripts/Pedestrian.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedestrian : MonoBehaviour {
+
+    public GameObject Target;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(Target.transform.position);
+    }
+}
