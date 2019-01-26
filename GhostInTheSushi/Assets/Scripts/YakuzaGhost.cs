774249e83b39fe7c73e0cd294df2d57c9d89497a
YianniR <conf270@soton.ac.uk>
2019-01-26 13:51:57 +0000

    Added playerMove script and yakuzaAI follow script

---
+++ GhostInTheSushi/Assets/Scripts/YakuzaGhost.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YakuzaGhost : MonoBehaviour
+{
+
+    public GameObject FamilyMember;
+
+    public float MaxDistance = 5;
+    public float DistanceToFamily;
+
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    public float FollowSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var heading = FamilyMember.transform.position - agent.transform.position;
+        DistanceToFamily = heading.magnitude;
+
+        if (DistanceToFamily <= MaxDistance)
+        {
+            agent.SetDestination(FamilyMember.transform.position);
+            agent.speed = FollowSpeed;
+            //YakuzaGhost.GetComponent<Animation>.Play("running");
+        }
+        else
+        {
+            agent.speed = 0;
+            //YakuzaGhost.GetComponent<Animation>.Play("idle");
+
+        }
+    }
+
+}
