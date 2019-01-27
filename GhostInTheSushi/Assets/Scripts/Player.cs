9af1ea4dffc94f6b54022fae2f0dd522e06bb672
charlie-wt <cwt1g14@soton.ac.uk>
2019-01-27 13:37:21 +0000

    vertical player movement with space/shift.

--- GhostInTheSushi/Assets/Scripts/Player.cs
+++ GhostInTheSushi/Assets/Scripts/Player.cs
@@ -5,9 +5,11 @@ using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(PhysicsRaycaster))]
 public class Player : MonoBehaviour {
-    public float moveSpeed = 0.1f;
+    public float horizMoveSpeed = 0.2f;
+    public float vertMoveSpeed = 0.15f;
     public float minX, width;
-    public float minZ, height;
+    public float minY, height;
+    public float minZ, depth;
     public float typhoonHoldTime = 3.0f;
     public float typhoonCooldown = 10.0f;
 
@@ -32,18 +34,27 @@ public class Player : MonoBehaviour {
         Vector3 vel = Vector3.zero;
 
         // get player input.
-        vel.x = Input.GetAxis("Horizontal") * moveSpeed;
-        vel.z = Input.GetAxis("Vertical") * moveSpeed;
+        vel.x = Input.GetAxis("Horizontal") * horizMoveSpeed;
+        vel.z = Input.GetAxis("Vertical") * horizMoveSpeed;
 
         // make sure we don't go faster when going diagonally.
-        vel = Vector3.ClampMagnitude(vel, moveSpeed);
+        vel = Vector3.ClampMagnitude(vel, horizMoveSpeed);
 
+        // up and down -- simpler, and doesn't affect the horizontal magnitude.
+        if (Input.GetKey(KeyCode.Space)) {
+            vel.y = vertMoveSpeed;
+        } else if (Input.GetKey(KeyCode.LeftShift)) {
+            vel.y = -vertMoveSpeed;
+        }
+
+        // apply the movement.
         Vector3 pos = transform.position;
         pos += vel;
 
         // make sure we don't exceed the set bounds.
         pos.x = Mathf.Clamp(pos.x, minX, minX + width);
-        pos.z = Mathf.Clamp(pos.z, minZ, minZ + height);
+        pos.y = Mathf.Clamp(pos.y, minY, minY + height);
+        pos.z = Mathf.Clamp(pos.z, minZ, minZ + depth);
 
         transform.position = pos;
     }
