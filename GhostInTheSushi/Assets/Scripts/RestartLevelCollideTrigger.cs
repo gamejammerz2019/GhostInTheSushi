958b5c6b0e721f50f0fa33bbe876a1e6a624ca92
Yiannis <yiannis.rav@gmail.com>
2019-01-27 15:06:05 +0000

    Improved ghost logic

--- GhostInTheSushi/Assets/Scripts/RestartLevelCollideTrigger.cs
+++ GhostInTheSushi/Assets/Scripts/RestartLevelCollideTrigger.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 using System.IO;
 
 public class RestartLevelCollideTrigger : MonoBehaviour {
+
+    public NavMeshAgent agent;
 
     private Scene scene;
     public string tag = "FamilyMember";
@@ -16,10 +19,20 @@ public class RestartLevelCollideTrigger : MonoBehaviour {
 	// Update is called once per frame
 	void OnTriggerEnter (Collider other) {
 
-        if (other.gameObject.tag == tag)
+        if (other.gameObject.tag == tag && agent.speed != 0.0f)
         {
             Debug.Log("Reload Level");
             Application.LoadLevel(scene.name);
         }	
 	}
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == tag && agent.speed != 0.0f)
+        {
+            Debug.Log("Reload Level");
+            Application.LoadLevel(scene.name);
+        }
+    }
 }
